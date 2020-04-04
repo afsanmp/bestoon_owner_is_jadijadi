54edb0e83ca1ee6f4604b61ceffaf1f6db6ad46c
cloner_21 <hosein.iup93@gmail.com>
2020-04-04 11:27:19 +0430

    income in models and some beautification

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Expense
+from .models import Expense, Incom
 
 # Register your models here.
 
 admin.site.register(Expense)
+admin.site.register(Incom)
--- web/models.py
+++ web/models.py
@@ -5,6 +5,18 @@ from django.contrib.auth.models import User
 
 class Expense(models.Model):
     text = models.CharField(max_length=255)
-    data = models.DateField()
+    date = models.DateField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    def __unicode__(self):
+        return "{}-{}".format(self.date, self.amount)
+
+
+
+class Incom(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    def __unicode__(self):
+        return "{}-{}".format(self.date, self.amount)
